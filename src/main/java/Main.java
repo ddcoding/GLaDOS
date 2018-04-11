de4709618d02f5ffb45d60587d14249601fca43f
Karol Drozdowski <karol@karol-VirtualBox>
2018-04-11 10:54:00 +0200

    Refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
     private static GLaDOSService gLaDOSService = new GLaDOSService();
 
     public static void main(String[] args) throws Exception {
-        gLaDOSService.startService();
+//        gLaDOSService.startService();
+        System.err.println("Hello world");
     }
 }
